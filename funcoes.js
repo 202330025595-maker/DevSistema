17896bd34d24adc4440837e0326bd1f47a67c1fc
Lara Beatriz <202330025595@estudante.rn.gov.br>
2025-11-13 13:44:24 +0000

    ajuste

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,15 @@
 function fazerlogin(){
-    let login = document.getElementById('login').ariaValueMax
-    let 
+    let login = document.getElementById('login').valuer
+    let senha = document.getElementById('senha').valuer
+
+    if(login === 'cenep' && senha === 'cenep123'){
+        alert('Login e senha Corretos')
+        localStorage.setItem('nome', login)
+        window.location.href = 'pagina2.html'
+    }
+
+
+
+
+
 }
